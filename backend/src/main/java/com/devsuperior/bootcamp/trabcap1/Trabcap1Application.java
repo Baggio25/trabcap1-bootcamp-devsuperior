d300e868ac07d31fb3a92fef6eb6b769c9aeb696
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-05-11 16:56:29 -0300

    First Commit

---
+++ backend/src/main/java/com/devsuperior/bootcamp/trabcap1/Trabcap1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.bootcamp.trabcap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabcap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabcap1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/bootcamp/trabcap1/Trabcap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.bootcamp.trabcap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabcap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
