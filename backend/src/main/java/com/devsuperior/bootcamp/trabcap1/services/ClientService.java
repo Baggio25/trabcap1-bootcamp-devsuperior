e98e79e068576b46c4d418f744f9f6bf1c4fdbd4
Baggio25 <rodrigo.baggio.si@gmail.com>
2021-05-11 22:33:55 -0300

    CRUD and Exceptions

--- backend/src/main/java/com/devsuperior/bootcamp/trabcap1/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/bootcamp/trabcap1/services/ClientService.java
@@ -1,6 +1,11 @@
 package com.devsuperior.bootcamp.trabcap1.services;
 
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -9,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.bootcamp.trabcap1.dto.ClientDTO;
 import com.devsuperior.bootcamp.trabcap1.entities.Client;
 import com.devsuperior.bootcamp.trabcap1.repositories.ClientRepository;
+import com.devsuperior.bootcamp.trabcap1.services.exception.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -21,5 +27,49 @@ public class ClientService {
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(cli -> new ClientDTO(cli));
 	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+	}
+	
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+	}
+	
 	
 }
